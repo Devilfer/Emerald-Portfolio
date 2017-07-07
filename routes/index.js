0160549e616d51432a55a244fa08035ca8365d56
Devilfer <sukhpartap55@gmail.com>
2017-07-07 13:23:47 -0400

    name as title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+let name = "Emerald Bricker";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: name });
 });
 
 module.exports = router;
